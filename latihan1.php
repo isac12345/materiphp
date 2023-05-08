c4b92e64ce3bff321fe3c741fd97e66332331e44
isac12345 <130331504+isac12345@users.noreply.github.com>
2023-05-08 11:27:59 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Percobaan PHP</title>
+</head>
+<body>
+
+    <?php echo "<p>Hello Word<p>"; ?>
+    <!-- echo untuk mencetak hasil -->
+    
+</body>
+</html>
